bb0a68a84e16f83d8b0c7a01b43f7d63b744e358
kenzo4610 <anshadsanshad0@gmail.com>
2026-05-18 08:08:55 +0530

    Update .puppeteerrc.cjs

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,8 +1,9 @@
-const {join} = require('path');
+const { join } = require('path');
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
+  // റെൻഡറിലെ റാം ലാഭിക്കാൻ വേണ്ടി കാഷെ ഫോൾഡർ സെറ്റ് ചെയ്യുന്നു
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
